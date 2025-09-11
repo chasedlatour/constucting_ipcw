f1d6a9bfdfb39fa3d708388f9b4b81b949053110
chasedlatour <chasedlatour@gmail.com>
2025-09-11 14:17:49 -0400

    Revised how creating dataset.

--- create_analytic_dataset.R
+++ create_analytic_dataset.R
@@ -31,24 +31,37 @@ setnames(df, c("id", "male", "black", "hispanic", "idu", "art", "delta", "drop",
 
 ###################################################################
 # Subset to those individuals in the control arm who were not LTFU
+# This will serve as the population in which we aim to estimate
+# risks with IPCW.
 
 dfs <- df[art == 0 & drop == 0]
 
 
 
 ###################################################################
+# We are interested in dichotomizing CD4 cell count to create
+# the variable `Z`. CD4 cell count is a good variable because it 
+# has a strong relationship with the outcome. 
+
+# Look at the distribution
+
+hist(dfs$cd4)
+summary(dfs$cd4)
+
+# The median is 68.5, so we discretize at that value
+
 # Dichotomize CD4 count as <60 and more than 60
 
-dfs[, cutoff := ifelse(cd4 < 60, 0, 1)]
+dfs[, cutoff := ifelse(cd4 < 68, 0, 1)]
 
-# Confirm that we se different risk of the outcome across the cutoff strata for CD4
+# Confirm that we see different risk of the outcome across the cutoff strata for CD4
 # Risk difference (using riskratio.wald from epitools for example)
 library(epitools)
 tab <- table(dfs$cutoff, dfs$delta)
 rr <- riskratio.wald(tab)  # includes RD in output
 print(rr)
 
-# See MUCH lower risk among those where CD4 is greater than or equal to 60
+# See MUCH lower risk among those where CD4 is greater than or equal to 68
 
 
 
@@ -58,7 +71,7 @@ print(rr)
 
 # Create a new value for censoring time for each person, where the average time to censoring is
 # differential by CD4.
-dfs[, days_c := rweibull(.N, shape = 0.5 + 2.9 * cutoff, scale = ifelse(cutoff == 1, 650, 850))]
+dfs[, days_c := rweibull(.N, shape = 1.5 + 2.5 * cutoff, scale = ifelse(cutoff == 1, 600, 850))]
 
 # Now create new values for tieme to event, outcome, and drop
 dfs[, t := ifelse(days_c < days, days_c, days)] # New time to outcome or censoring
